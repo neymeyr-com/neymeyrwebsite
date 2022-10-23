b887256ce024a8433c2bcc3c554a3b00f9a6ecf9
neymeyr-com <115020732+neymeyr-com@users.noreply.github.com>
2022-10-23 23:18:40 +0200

    Add files via upload

---
+++ game3/script.js
@@ -0,0 +1,16 @@
+var points = 0;
+var increment = 1;
+var shopCount = 10;
+
+const counterEle = document.querySelector('#counter');
+const shopCounterEle = document.querySelector('#shopCount');
+
+function klickMich() {
+    points += increment;
+    counterEle.innerHTML = points;
+}
+
+function shop() {
+    alert('Hallo!')
+
+}
