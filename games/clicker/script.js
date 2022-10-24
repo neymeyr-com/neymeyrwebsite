7a6393cb678aaa29b48fe17b012f5f42f0b6562e
neymeyr-com <115020732+neymeyr-com@users.noreply.github.com>
2022-10-24 19:05:40 +0200

    Add files via upload

---
+++ games/clicker/script.js
@@ -0,0 +1,20 @@
+var points = 0;
+var increment = 1;
+var shopCount = 10;
+
+const counterEle = document.querySelector('#counter');
+const shopCounterEle = document.querySelector('#shopCount');
+
+function klickMich() {
+    points += increment;
+    counterEle.innerHTML = points;
+}
+
+function shop() {
+    alert('Hallo!')
+
+}
+
+window.onbeforeunload = function() {
+    return "";
+  }
